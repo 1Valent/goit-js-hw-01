20c9d59b32607feaa0012803e272bd36362b78c1
Valent Pron <valentynschaposchnik@gmail.com>
2024-04-17 12:59:46 +0100

    ff

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,13 @@
+const num1 = "Hi";
+
+function makeTransaction(num, price) {
+    let sum = num1 + price;
+    return sum;
+}
+
+
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(num1);
