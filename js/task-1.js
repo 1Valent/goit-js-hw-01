94c794a1ef2f9788487725db1c00570dfe871a39
Valent Pron <valentynschaposchnik@gmail.com>
2024-04-19 02:17:47 +0100

    2

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,3 @@
-const num1 = "Hi";
-
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
